8846fdd6ba7a8b5aca5524bca959a89ea60dd754
Shin-ichi Ueda <skyriser@levolution.info>
2023-06-19 11:25:09 +0900

    add paces.jp finished status hiding script

---
+++ paces_jp/remove_finished_projects.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name         Pace.jp / Remove finish status projects from select boxes
+// @namespace    https://app.paces.jp/report/
+// @version      1.0
+// @description  "検収済" となっているプロジェクトを非表示にする
+// @match        https://app.paces.jp/report*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  function hideProjects() {
+    var divElements = document.querySelectorAll('div > span.ds_tag');
+
+    for (var i = 0; i < divElements.length; i++) {
+        var divElement = divElements[i];
+        var liElement = divElement.parentNode;
+
+        while (liElement && liElement.tagName !== 'LI') {
+            liElement = liElement.parentNode;
+        }
+
+        if (liElement) {
+            liElement.style.display = 'none';
+        }
+    }
+  };
+
+  window.addEventListener('click', hideProjects);
+})();
